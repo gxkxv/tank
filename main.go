562a4cd094db2110e1f6585854794f8d1f955f22
gxkxv <diaaxe.18@gmail.com>
2025-05-06 16:49:53 +0800

    add some comments, move the code on new version of ebiten, add beta version of tank, fix some mistakes

--- main.go
+++ main.go
@@ -1,30 +1,37 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 	"image/color"
 	"log"
 )
 
 type Game struct{}
 
-func (g *Game) Update(screen *ebiten.Image) error {
+func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.Black)
+	ebitenutil.DebugPrint(screen, "Tank Game!") // shows the text
+	tankColor := color.RGBA{255, 0, 0, 255}
+	vector.DrawFilledCircle(screen, 50, 50, 30, tankColor, false) //draw a circle
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return 800, 600
+	return 800, 600 // screen size
 }
 func main() {
 	g := &Game{}
 
+	// set window size
 	ebiten.SetWindowSize(800, 600)
-	ebiten.SetWindowResizable(false)
 	ebiten.SetWindowTitle("Tank Game")
+
+	// start the game
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
